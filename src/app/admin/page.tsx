373834ac405442709838309fe919dcf87fa4f680
Your Name <azamatabraev07@gmail.com>
2023-11-14 21:05:05 +0500

    testing completed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import protectedPage from "@/hoc/with-auth";
-
 const DashboardPage = () => {
   return <div>DashboardPage</div>;
 };
 
-export default protectedPage(DashboardPage);
+export default DashboardPage;
