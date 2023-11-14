bceeab9e5b64c291d3619fdf2a60643a7f5b8fec
Your Name <azamatabraev07@gmail.com>
2023-11-14 17:14:21 +0500

    testing

--- src/hoc/with-auth.tsx
+++ src/hoc/with-auth.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import React from "react";
 import useAuth from "@/store/auth"
@@ -5,24 +6,24 @@ import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 import { useEffect } from "react";
 
-function protectedPage<P extends React.JSX.IntrinsicAttributes>(Component: React.ComponentType<P>){
-  const AuthComponent = (props: P) => {
-    const {isAuthenticated, user} = useAuth();
+function protectedPage<P extends React.JSX.IntrinsicAttributes>(Component: React.ComponentType<P>) {
+  const AuthComponent = (props: React.ComponentProps<typeof Component>) => {
+    const { isAuthenticated, user } = useAuth();
     const router = useRouter();
     const pathname = usePathname();
 
     useEffect(() => {
       if (isAuthenticated) {
         if (pathname.startsWith("/admin") && user?.role === 0) {
-          router.push("/login")
+          router.push("/login");
         }
       } else {
         router.push("/login");
       }
-    }, [isAuthenticated, router, pathname, user])
+    }, [isAuthenticated, router, pathname, user]);
 
-    return <Component {...props}/>
-  }
+    return <Component {...props} />;
+  };
 
   return AuthComponent;
 }
